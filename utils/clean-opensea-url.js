7d9951d17c7b0b1b67ec1c48cb641916e3bf5d2b
Freddy González <7684330+fredoist@users.noreply.github.com>
2023-04-30 12:23:18 -0600

    fix opensea url cleaning

--- utils/clean-opensea-url.js
+++ utils/clean-opensea-url.js
@@ -1,7 +1,6 @@
 export function cleanUrl(url) {
   if (url) {
-    // remove https://opensea.io/assets/ and locales from token
-    const regex = /https:\/\/opensea.io\/assets\/([a-z]{2}\/)?/g;
+    const regex = /https:\/\/opensea.io\/([a-z]{2}\/)?assets\//g;
     const tokenString = url.trim().replace(regex, '').split('?')[0];
     return tokenString;
   }
